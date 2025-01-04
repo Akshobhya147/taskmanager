dd300525cab6d0a0d684fef65bb9b24e5774f079
Akshobhya <akshobhyashukla@gmail.com>
2025-01-04 15:25:57 +0530

    fifth commit

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,5 +1,5 @@
 import { toast, TypeOptions } from "react-toastify";
-export const API_URL = "http://localhost:8080";
+export const API_URL = "https://taskmanager-backend-wine.vercel.app";
 export const makingToast = (message: String, type: TypeOptions) => {
   toast(message, { type: type });
 };
